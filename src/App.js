f600aa86e177382c64dbd1574707576d4dc55ba1
NatalieSokolova <nataliesklv@gmail.com>
2020-11-04 17:20:20 +0000

    adds items props to accordion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a frontend JS framework",
+  },
+
+  {
+    title: "What use React?",
+    content: "Because React is amazing!",
+  },
+
+  {
+    title: "How do you use React?",
+    content: "We use React by creating components",
+  },
+];
+
 export default () => {
   return (
     <div>
-      <Accordion />
+      <Accordion items={items} />
     </div>
   );
 };
